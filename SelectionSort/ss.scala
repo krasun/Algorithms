ca5549ebd316e502113e0b9e00efc1c8c9515947
Dmitry Krasun <krasun.net@gmail.com>
2016-02-23 09:33:31 +0200

    Ooops...

--- SelectionSort/ss.scala
+++ SelectionSort/ss.scala
@@ -1,19 +1,25 @@
-// https://en.wikipedia.org/wiki/Insertion_sort
+// https://en.wikipedia.org/wiki/Selection_sort
 
-def insertionSort(array: Array[Int]): Unit = {
-  for (index <- 1 to (array.length - 1)) {
-    val key = array(index)
-    var j = index - 1
-    while (j > 0 && array(j) > key) {
-      array(j + 1) = array(j)
-      j = j - 1
+def selectionSort(array: Array[Int]): Unit = {
+  for (index <- 0 to (array.length - 2)) {
+    var smallestIndex = index
+
+    // find smallest index
+    for (restIndex <- (index + 1) to (array.length - 1)) {
+      if (array(restIndex) < array(smallestIndex)) {
+        smallestIndex = restIndex
+      }
     }
-    array(j + 1) = key
+
+    // exchange current element with smallest
+    val temporary = array(index)
+    array(index) = array(smallestIndex)
+    array(smallestIndex) = temporary
   }
 }
 
 val array = Array(5, 4, 3, 2, 1)
-insertionSort(array)
+selectionSort(array)
 
 println(
   array.sameElements(Array(1, 2, 3, 4, 5))
