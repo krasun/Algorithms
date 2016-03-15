dfed6088921df1f29a02eda800c74ced1b0b6997
Dmitry Krasun <krasun.net@gmail.com>
2016-03-15 22:28:59 +0200

    Add algorithm for finding negative weight cycle

--- BellmanFord/bf.scala
+++ BellmanFord/bf.scala
@@ -1,6 +1,8 @@
-// https://en.wikipedia.org/wiki/Bellman%E2%80%93Ford_algorithm
+// https://en.wikipedia.org/wiki/Bellman-Ford_algorithm
 
-def bellmanFord(graph: Array[Array[Int]], startVertex: Int): (Array[Double], Array[Option[Int]]) = {
+import scala.collection.mutable
+
+def bellmanFord(graph: Array[Array[Int]], startVertex: Int): (Array[Double], Array[Option[Int]], Seq[Int]) = {
 
   def initializePaths(vertices: Range, startVertex: Int): (Array[Double], Array[Option[Int]]) = {
     val shortest = new Array[Double](vertices.length)
@@ -17,16 +19,42 @@ def bellmanFord(graph: Array[Array[Int]], startVertex: Int): (Array[Double], Arr
     (shortest, prev)
   }
 
-  def edges(graph: Array[Array[Int]]): Seq[(Int, Int)] =
-    for (u <- graph.indices; v <- graph(u).indices; if 0 != graph(u)(v))
-      yield (u, v)
-
   def relax(graph: Array[Array[Int]], u: Int, v: Int, shortest: Array[Double], prev: Array[Option[Int]]) {
     val weight = graph(u)(v)
     if ((shortest(u) + weight) < shortest(v)) {
       shortest(v) = shortest(u) + weight
       prev(v) = Some(u)
     }
+  }
+
+  def edges(graph: Array[Array[Int]]): Seq[(Int, Int)] =
+    for (u <- graph.indices; v <- graph(u).indices; if 0 != graph(u)(v))
+      yield (u, v)
+
+
+  def findNegativeWeightCycle(graph: Array[Array[Int]], shortest: Array[Double], prev: Array[Option[Int]]): Seq[Int] = {
+    var cycle = mutable.MutableList[Int]()
+
+    for ((u, v) <- edges(graph); if (shortest(u) + graph(u)(v)) < shortest(v)) {
+      val visited = new Array[Boolean](graph.length)
+
+      var x = v
+      while (! visited(x)) {
+        visited(x) = true
+        x = prev(x).get
+      }
+
+      var vertexInCycle = prev(x).get
+      cycle += x
+      while (vertexInCycle != x) {
+        cycle = mutable.MutableList[Int](vertexInCycle) ++ cycle
+        vertexInCycle = prev(vertexInCycle).get
+      }
+
+      return cycle
+    }
+
+    return cycle
   }
 
   val (shortest, prev) = initializePaths(graph.indices, startVertex)
@@ -37,8 +65,9 @@ def bellmanFord(graph: Array[Array[Int]], startVertex: Int): (Array[Double], Arr
     }
   }
 
-  (shortest, prev)
+  (shortest, prev, findNegativeWeightCycle(graph, shortest, prev))
 }
+
 
 val graph = Array(
   Array(0, 6, 7, 0, 0),
@@ -48,10 +77,33 @@ val graph = Array(
   Array(2, 0, 0, 7, 0)
 )
 
-val (shortest, prev) = bellmanFord(graph, 0)
+val (shortest, prev, emptyNegativeWeightCycle) = bellmanFord(graph, 0)
+
+println("Results without negative cycle:")
 
 println(shortest.mkString(" "))
 println(prev.mkString(" "))
+println(emptyNegativeWeightCycle.mkString(" "))
 
+println(emptyNegativeWeightCycle.isEmpty)
 println(shortest.sameElements(Array(0.0, 2.0, 7.0, 4.0, -2.0)))
 println(prev.sameElements(Array(None, Some(3), Some(0), Some(2), Some(1))))
+
+
+val graphWithNegativeCycle = Array(
+  Array(0, 1, 0),
+  Array(0, 0, -1),
+  Array(1, -1, 0)
+)
+
+val (shortestNegativeCycle, prevNegativeCycle, negativeWeightCycle) = bellmanFord(graphWithNegativeCycle, 0)
+
+println("Results with negative cycle:")
+
+println(shortestNegativeCycle.mkString(" "))
+println(prevNegativeCycle.mkString(" "))
+println(negativeWeightCycle.mkString(" "))
+
+println(negativeWeightCycle.isEmpty)
+println(shortestNegativeCycle.sameElements(Array(0.0, 2.0, 7.0, 4.0, -2.0)))
+println(prevNegativeCycle.sameElements(Array(None, Some(3), Some(0), Some(2), Some(1))))
